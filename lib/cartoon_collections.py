3e689fcb6ccc87b45d54d08e09e06e7b0a42dd79
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-11-27 15:55:46 +0300

    cmplt lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+# Function to print out each dwarf name in number order
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, start=1):
+        print(f"{i}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+# Function to capitalize each planeteer call and add an exclamation point
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + '!' for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+# Function to check if any planeteer call is longer than four characters
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
 
-def find_the_cheese():
-    pass
+# Function to find and return the first string that is a type of cheese
+def find_the_cheese(snacks):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for item in snacks:
+        if item in cheeses:
+            return item
+    return None
